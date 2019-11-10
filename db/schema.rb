8c8fc5411976ae116d73af17753f5e347f32bb02
Randel Jiao <randeljiao@gmail.com>
2019-11-10 03:41:08 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191110033100) do
+ActiveRecord::Schema.define(version: 20191110034047) do
 
- 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
     t.string   "payment_status"
@@ -40,7 +39,7 @@ ActiveRecord::Schema.define(version: 20191110033100) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "post_status"
+    t.boolean  "post_status"
   end
 
 end
